e6916270449b1e569e43675e63d0f6a90a698979
Kelvin Lee <leekelvin0225@gmail.com>
2020-07-16 21:21:52 -0400

    Added Tests and Class

---
+++ CalculatorRadiation/Radiation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculatorRadiation
+{
+    public class Radiation
+    {
+        public double Calc(int x)
+        {
+            double result = Math.Sqrt(x);
+            return result;
+        }
+    }
+}
